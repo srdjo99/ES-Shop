e8786c398fa9fc53b28c6d26ba1e3487ed19cb71
Srdjan <srdjancoralic5@gmail.com>
2023-02-06 20:20:29 +0100

    add basic layout

--- components/layout/ShopLayout.tsx
+++ components/layout/ShopLayout.tsx
@@ -1,1 +1,33 @@
-randomFillSync;
+import { FC, PropsWithChildren } from 'react';
+import Head from 'next/head';
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+}
+
+export const ShopLayout: FC<PropsWithChildren<Props>> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <nav>{/* navbar */}</nav>
+      {/* sidebar */}
+
+      <main
+        style={{ margin: '80px auto', maxWidth: '1440px', padding: '0px 30px' }}
+      >
+        {children}
+      </main>
+
+      <footer>{/* custom footer */}</footer>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layout/ShopLayout';
 
 const Home: NextPage = () => {
-  return <h1>ES - Shop</h1>;
+  return (
+    <ShopLayout
+      title='ES-Shop - Home '
+      pageDescription='Find products suitable just for you'
+    >
+      <Typography variant='h1' component='h1'>
+        Home
+      </Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>
+        All the products
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;

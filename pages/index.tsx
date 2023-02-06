f81ad8abb64c92b8664573173b08211d30e514cc
Srdjan <srdjancoralic5@gmail.com>
2023-02-06 21:43:58 +0100

    add products temporarily to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import {
+  Typography,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+} from '@mui/material';
+
 import { ShopLayout } from '../components/layout/ShopLayout';
+// temporarly
+import { initialData } from '../database/products';
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +23,22 @@ const Home: NextPage = () => {
       <Typography variant='h2' sx={{ mb: 1 }}>
         All the products
       </Typography>
+
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item key={product.slug} xs={6} sm={4}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  image={`products/${product.images[0]}`}
+                  alt={product.title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   );
 };

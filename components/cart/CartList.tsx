4e9e6129fe1d4855105e0dd983e5e1c4ca16cc30
Srdjan <srdjancoralic5@gmail.com>
2023-02-07 14:12:24 +0100

    add cart list

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,5 +1,16 @@
-import { Typography } from '@mui/material';
+import NextLink from 'next/link';
+import {
+  CardActionArea,
+  Grid,
+  Link,
+  Typography,
+  CardMedia,
+  Box,
+  Button,
+} from '@mui/material';
+
 import { initialData } from '../../database/products';
+import { ItemCounter } from '../ui';
 
 const productsInCart = [
   initialData.products[0],
@@ -11,7 +22,42 @@ export const CartList = () => {
   return (
     <>
       {productsInCart.map((product) => (
-        <Typography key={product.slug}>{product.title}</Typography>
+        <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
+          <Grid item xs={3}>
+            <NextLink href='/product/slug' passHref>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`products/${product.images[0]}`}
+                    component='img'
+                    sx={{ borderRadius: '5px' }}
+                  />
+                </CardActionArea>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid item xs={7}>
+            <Box display='flex' flexDirection='column'>
+              <Typography variant='body1'>{product.title}</Typography>
+              <Typography variant='body1'>
+                Size: <strong>M</strong>
+              </Typography>
+              <ItemCounter />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            display='flex'
+            alignItems='center'
+            flexDirection='column'
+          >
+            <Typography variant='subtitle1'>{`$${product.price}`}</Typography>
+            <Button variant='text' color='secondary'>
+              Remove
+            </Button>
+          </Grid>
+        </Grid>
       ))}
     </>
   );

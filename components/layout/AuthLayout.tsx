d5fbe0711e99dbc39af5bba2156bdeccf924dbbc
Srdjan <srdjancoralic5@gmail.com>
2023-02-09 21:46:37 +0100

    add login and register pages

---
+++ components/layout/AuthLayout.tsx
@@ -0,0 +1,31 @@
+import { FC, PropsWithChildren } from 'react';
+import Head from 'next/head';
+import { Box } from '@mui/material';
+
+interface Props {
+  title: string;
+}
+
+export const AuthLayout: FC<PropsWithChildren<Props>> = ({
+  children,
+  title,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <main>
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          height='calc(100vh - 200px) '
+        >
+          {children}
+        </Box>
+      </main>
+    </>
+  );
+};
---
+++ pages/auth/register.tsx
@@ -0,0 +1,50 @@
+import NextLink from 'next/link';
+import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../../components/layout';
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout title='Register'>
+      <Box sx={{ width: 350, padding: '10px 20px' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant='h1' component='h1'>
+              Create an account
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Full name' variant='filled' fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Email' variant='filled' fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label='Password'
+              type='password'
+              variant='filled'
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              color='secondary'
+              className='circular-btn'
+              size='large'
+              fullWidth
+            >
+              Register
+            </Button>
+          </Grid>
+          <Grid item xs={12} display='flex' justifyContent='end'>
+            <NextLink href='/auth/login' passHref>
+              <Link underline='always'>You already have an account?</Link>
+            </NextLink>
+          </Grid>
+        </Grid>
+      </Box>
+    </AuthLayout>
+  );
+};
+
+export default RegisterPage;

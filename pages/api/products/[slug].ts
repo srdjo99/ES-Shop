78ca24f0e3782e6c1284d64e7c68ae3783f49c4f
Srdjan <srdjancoralic5@gmail.com>
2023-02-10 15:58:39 +0100

    getting product by slug functionality

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { IProduct } from '../../../interfaces';
+import { Product } from '../../../models';
+
+type Data =
+  | {
+      message: string;
+    }
+  | IProduct;
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET':
+      return getProductBySlug(req, res);
+
+    default:
+      return res.status(400).json({ message: 'Bad Request' });
+  }
+}
+
+const getProductBySlug = async (req: NextApiRequest, res: NextApiResponse) => {
+  await db.connect();
+  const { slug } = req.query;
+  const product = await Product.findOne({ slug });
+  await db.disconnect();
+
+  if (!product) return res.status(404).json({ message: 'Product not found' });
+
+  return res.json({ product });
+};

516dc9fc57405b892a615162b29079aa36d41d58
Srdjan <srdjancoralic5@gmail.com>
2023-02-13 12:13:52 +0100

    add loader for each card

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -18,6 +18,7 @@ interface Props {
 
 export const ProductCard: FC<Props> = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const productImage = useMemo(() => {
     return isHovered
@@ -42,13 +43,17 @@ export const ProductCard: FC<Props> = ({ product }) => {
                 className='fadeIn'
                 image={productImage}
                 alt={product.title}
+                onLoad={() => setIsImageLoaded(true)}
               />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className='fadeIn'>
+      <Box
+        sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none' }}
+        className='fadeIn'
+      >
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>{`$${product.price}`}</Typography>
       </Box>

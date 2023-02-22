508155345b196e4e8ee3d01590d466b76bc835d9
Srdjan <srdjancoralic5@gmail.com>
2023-02-22 11:11:18 +0100

    add order pricing summary

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -36,6 +36,25 @@ export const CartProvider: FC<any> = ({ children }) => {
     Cookie.set('cart', JSON.stringify(state.cart));
   }, [state.cart]);
 
+  useEffect(() => {
+    const numberOfItems = state.cart.reduce(
+      (prev, current) => current.quantity + prev,
+      0
+    );
+    const subTotal = state.cart.reduce(
+      (prev, current) => current.price * current.quantity + prev,
+      0
+    );
+    const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+
+    const orderSummary = {
+      numberOfItems,
+      subTotal,
+      tax: subTotal * taxRate,
+      total: subTotal * (taxRate + 1),
+    };
+  }, [state.cart]);
+
   const addProductToCart = (product: ICartProduct) => {
     const productInCart = state.cart.some((p) => p._id === product._id);
     if (productInCart)

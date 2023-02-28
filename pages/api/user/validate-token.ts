537f707a4e83163076c1459d2328d7e43d679d4e
Srdjan <srdjancoralic5@gmail.com>
2023-02-28 10:17:51 +0100

    add token validator function

--- pages/api/user/validate-token.ts
+++ pages/api/user/validate-token.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import bcrypt from 'bcryptjs';
 import { db } from '../../../database';
+import { jwt } from '../../../utils';
+import { User } from '../../../models';
 
 type Data =
   | { message: string }
@@ -21,5 +23,28 @@ export default function handler(
 const checkJWT = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const { token = '' } = req.cookies;
 
+  let userId = '';
+
+  try {
+    userId = await jwt.isValidToken(token);
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: 'Authorization token is not valid' });
+  }
+
   await db.connect();
+  const user = await User.findById(userId).lean();
+  await db.disconnect();
+
+  if (!user) {
+    return res.status(400).json({ message: 'There is no user with that id' });
+  }
+
+  const { _id, email, role, name } = user;
+
+  return res.status(200).json({
+    token: jwt.signToken(_id, email),
+    user: { email, role, name },
+  });
 };

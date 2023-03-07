0c74135a8b28e1a4aa11971f85990814e71b3dc8
Srdjan <srdjancoralic5@gmail.com>
2023-03-07 09:53:11 +0100

    testing next-auth

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -1,5 +1,7 @@
 import { FC, useReducer, ReactNode, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
 import Cookies from 'js-cookie';
 import axios from 'axios';
 
@@ -19,11 +21,18 @@ const AUTH_INITIAL_STATE: AuthState = {
 
 export const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+  const { data, status } = useSession();
   const router = useRouter();
 
   useEffect(() => {
-    checkToken();
-  }, []);
+    if (status === 'authenticated') {
+      // dispatch({ type: '[Auth] - Login', payload: data?.user as IUser });
+    }
+  }, [status, data]);
+
+  // useEffect(() => {
+  //   checkToken();
+  // }, []);
 
   const checkToken = async () => {
     if (!Cookies.get('token')) return;

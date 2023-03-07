eab412d1715f5e2d67ec48c99476d686e6e46123
Srdjan <srdjancoralic5@gmail.com>
2023-03-07 09:38:54 +0100

    add next-auth and github as a provider

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -89,6 +89,15 @@ export const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const logout = () => {
     Cookies.remove('token');
     Cookies.remove('cart');
+    Cookies.remove('firstName');
+    Cookies.remove('lastName');
+    Cookies.remove('address');
+    Cookies.remove('address2');
+    Cookies.remove('zip');
+    Cookies.remove('city');
+    Cookies.remove('country');
+    Cookies.remove('phone');
+
     router.reload();
   };
 

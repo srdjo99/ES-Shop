07249633b481c6e5bca5c98e26d80cb951d9b1d0
Srdjan <srdjancoralic5@gmail.com>
2023-03-07 12:40:12 +0100

    providing credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,34 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/credentials';
 
 export default NextAuth({
   providers: [
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'email@gmail.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password',
+        },
+      },
+      async authorize(credentials) {
+        console.log({ credentials });
+        return null;
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+
+  // Callbacks
+  callbacks: {},
 });

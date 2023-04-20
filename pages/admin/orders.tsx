398c9ef86d86f14c5f90154a35db62ec7dad2aa3
Srdjan <srdjancoralic5@gmail.com>
2023-04-20 12:29:32 +0200

    add table for showing orders

--- pages/admin/orders.tsx
+++ pages/admin/orders.tsx
@@ -1,13 +1,80 @@
+import { Chip, Grid } from '@mui/material';
 import { ConfirmationNumberOutlined } from '@mui/icons-material';
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+
+import useSWR from 'swr';
+
 import { AdminLayout } from '../../components/layout';
+import { IOrder, IUser } from '../../interfaces';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'Order ID', width: 250 },
+  { field: 'email', headerName: 'Email', width: 250 },
+  { field: 'name', headerName: 'Full name', width: 300 },
+  { field: 'total', headerName: 'Total amount', width: 300 },
+  {
+    field: 'isPaid',
+    headerName: 'Paid',
+    renderCell: ({ row }: GridRenderCellParams) => {
+      return row.isPaid ? (
+        <Chip variant='outlined' label='Paid' color='success' />
+      ) : (
+        <Chip variant='outlined' label='Pending' color='error' />
+      );
+    },
+  },
+  {
+    field: 'noProducts',
+    headerName: 'No.Products',
+    align: 'center',
+    width: 120,
+  },
+  {
+    field: 'check',
+    headerName: 'See order',
+    renderCell: ({ row }: GridRenderCellParams) => {
+      return (
+        <a href={`/admin/orders/${row.id}`} target='_blank' rel='noreferrer'>
+          See order
+        </a>
+      );
+    },
+  },
+  { field: 'createdAt', headerName: 'Created at', width: 300 },
+];
 
 const OrdersPage = () => {
+  const { data, error } = useSWR<IOrder[]>('/api/admin/orders');
+
+  if (!data && !error) return <></>;
+
+  const rows = data!.map((order) => ({
+    id: order._id,
+    email: (order.user as IUser).email,
+    name: (order.user as IUser).name,
+    total: order.total,
+    isPaid: order.isPaid,
+    noProducts: order.numberOfItems,
+    createdAt: order.createdAt,
+  }));
+
   return (
     <AdminLayout
       title='Orders'
       subTitle='Order management'
       icon={<ConfirmationNumberOutlined />}
-    ></AdminLayout>
+    >
+      <Grid container className='fadeIn'>
+        <Grid item xs={12} sx={{ height: 650, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+          />
+        </Grid>
+      </Grid>
+    </AdminLayout>
   );
 };
 

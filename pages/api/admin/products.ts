8f8da79e4292deaa3cb7f9abe58bfe456032cad5
Srdjan <srdjancoralic5@gmail.com>
2023-04-21 14:45:00 +0200

    add api endpoint for updating product

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -2,8 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { IProduct } from '../../../interfaces';
 import { db } from '../../../database';
 import { Product } from '../../../models';
+import { isValidObjectId } from 'mongoose';
 
-type Data = { message: string } | IProduct[];
+type Data = { message: string } | IProduct[] | IProduct;
 
 export default function handler(
   req: NextApiRequest,
@@ -13,6 +14,7 @@ export default function handler(
     case 'GET':
       return getProducts(req, res);
     case 'PUT':
+      return updateProduct(req, res);
     case 'POST':
 
     default:
@@ -26,4 +28,40 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   await db.disconnect();
 
   return res.status(200).json(products);
+};
+
+const updateProduct = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const { _id = '', images = [] } = req.body as IProduct;
+
+  if (!isValidObjectId(_id)) {
+    return res.status(400).json({ message: 'Product ID is not valid' });
+  }
+
+  if (images.length <= 2) {
+    return res.status(400).json({ message: 'Minimum 2 images necessary' });
+  }
+
+  try {
+    await db.connect();
+
+    const product = await Product.findById(_id);
+    if (!product) {
+      await db.disconnect();
+      return res
+        .status(400)
+        .json({ message: 'Product with that ID does not exist' });
+    }
+
+    await product.update(req.body);
+    await db.disconnect();
+
+    return res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    await db.disconnect();
+    return res.status(400).json({ message: 'Check server console' });
+  }
 };

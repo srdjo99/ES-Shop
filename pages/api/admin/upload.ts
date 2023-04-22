b9be09433d2262d98121710412877a1d7d199455
Srdjan <srdjancoralic5@gmail.com>
2023-04-22 12:19:31 +0200

    TEMPORARY TEST  saving images to public folder

--- pages/api/admin/upload.ts
+++ pages/api/admin/upload.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import formidable from 'formidable';
+import fs from 'fs';
 
 type Data = { message: string };
 
@@ -21,7 +22,28 @@ export default function handler(
   }
 }
 
-const parseFiles = async (req: NextApiRequest) => {};
+const saveFile = async (file: formidable.File) => {
+  const data = fs.readFileSync(file.filepath);
+  fs.writeFileSync(`./public/${file.originalFilename}`, data);
+  fs.unlinkSync(file.filepath);
+  return;
+};
+
+const parseFiles = async (req: NextApiRequest) => {
+  return new Promise((resolve, reject) => {
+    const form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+      console.log({ err, fields, files });
+
+      if (err) {
+        return reject(err);
+      }
+
+      await saveFile(files.file as formidable.File);
+      resolve(true);
+    });
+  });
+};
 
 const uploadFile = async (req: NextApiRequest, res: NextApiResponse) => {
   await parseFiles(req);
